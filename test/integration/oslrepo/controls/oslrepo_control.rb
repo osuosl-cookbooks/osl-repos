1d2725d957920edd958a8643b920ec9d4c49bf20
Ethan Hampton <hamptone@osuosl.org>
2023-01-27 10:49:50 -0800

    Fix oslrepo tests to support AlmaLinux

--- test/integration/oslrepo/controls/oslrepo_control.rb
+++ test/integration/oslrepo/controls/oslrepo_control.rb
@@ -3,11 +3,12 @@ control 'oslrepo' do
 
   rel = os[:release].to_i
   arch = os[:arch]
+  platform = os[:name]
 
   describe yum.repo('osl') do
     it { should exist }
     it { should be_enabled }
-    its('baseurl') { should include "http://packages.osuosl.org/repositories/centos-#{rel}/osl/#{arch}" }
+    its('baseurl') { should include "http://packages.osuosl.org/repositories/#{platform}-#{rel}/osl/#{arch}" }
   end
 
   describe file('/etc/yum.repos.d/osl.repo') do
@@ -15,7 +16,7 @@ control 'oslrepo' do
   end
 
   describe ini('/etc/yum.repos.d/osl.repo') do
-    its('osl.baseurl') { should cmp 'http://packages.osuosl.org/repositories/centos-$releasever/osl/$basearch' }
+    its('osl.baseurl') { should cmp "http://packages.osuosl.org/repositories/#{platform}-$releasever/osl/$basearch" }
     its('osl.gpgcheck') { should cmp '0' }
   end
 end
