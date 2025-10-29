899ee8c50145e6ffe092d74f0917e831748a50e8
Ally Aoki <aokial@olive.workstation.cass.oregonstate.edu>
2025-10-29 16:13:07 -0700

    Debian 13 Integration

--- Berksfile
+++ Berksfile
@@ -1,4 +1,5 @@
 source 'https://supermarket.chef.io'
+source 'https://supermarket.osuosl.org'
 
 cookbook 'apt'
 cookbook 'osl-repos-test', path: 'test/cookbooks/osl-repos-test'
--- test/integration/debian/controls/debian.rb
+++ test/integration/debian/controls/debian.rb
@@ -1,18 +1,40 @@
 control 'debian' do
-  describe file '/etc/apt/sources.list' do
-    its('content') do
-      should cmp <<~EOF
-        # sources.list file written by Chef
-        deb https://debian.osuosl.org/debian bookworm main non-free non-free-firmware contrib
-        deb https://debian.osuosl.org/debian bookworm-updates main non-free non-free-firmware contrib
-        deb https://debian.osuosl.org/debian bookworm-backports main non-free non-free-firmware contrib
-        deb https://deb.debian.org/debian-security bookworm-security main non-free non-free-firmware contrib
-      EOF
+  if os.release.to_i == 12
+    describe file '/etc/apt/sources.list' do
+      its('content') do
+        should cmp <<~EOF
+          # sources.list file written by Chef
+          deb https://debian.osuosl.org/debian bookworm main non-free non-free-firmware contrib
+          deb https://debian.osuosl.org/debian bookworm-updates main non-free non-free-firmware contrib
+          deb https://debian.osuosl.org/debian bookworm-backports main non-free non-free-firmware contrib
+          deb https://deb.debian.org/debian-security bookworm-security main non-free non-free-firmware contrib
+        EOF
+      end
+    end
+  end
+  if os.release.to_i == 13
+    describe file '/etc/apt/sources.list' do
+      its('content') do
+        should cmp <<~EOF
+          # sources.list file written by Chef
+          deb https://debian.osuosl.org/debian trixie main non-free non-free-firmware contrib
+          deb https://debian.osuosl.org/debian trixie-updates main non-free non-free-firmware contrib
+          deb https://debian.osuosl.org/debian trixie-backports main non-free non-free-firmware contrib
+        EOF
+      end
     end
   end
 
-  describe file '/etc/apt/apt.conf.d/99osuosl' do
-    it { should_not exist }
+  if os.release.to_i == 12
+    describe file '/etc/apt/apt.conf.d/99osuosl' do
+      it { should_not exist }
+    end
+  end
+  if os.release.to_i == 13
+    describe file '/etc/apt/apt.conf.d/99osuosl' do
+      it { should exist }
+      its('content') { should match('APT::Default-Release "trixie";') }
+    end
   end
 
   %w(
@@ -25,7 +47,7 @@ control 'debian' do
   end
 
   describe file '/etc/apt/apt.conf.d/50unattended-upgrades' do
-    its('content') { should match /file written by Chef/ }
+    its('content') { should match(/file written by Chef/) }
   end
 
   %w(
